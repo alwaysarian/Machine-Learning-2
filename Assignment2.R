479a44149b3a0dca60e12c0c2cdabc06811834ad
Praveen Kumar Neelappa <pnkumar@outlook.com>
2017-09-13 16:34:45 -0400

    updated code

--- Assignment2.R
+++ Assignment2.R
@@ -1,7 +1,28 @@
 #All required library
 install.packages("caret")
 library(caret)
+install.packages("e1071")
+library("e1071")
 
 
-SA <-read.csv('http://math.mercyhurst.edu/~sousley/STAT_139/data/sahdd.csv', as.is = T);
+sa <-read.csv('http://math.mercyhurst.edu/~sousley/STAT_139/data/sahdd.csv', as.is = T);
 head(sa)
+attach(sa)
+
+chdfit <- glm(as.factor(chd)~ sbp+tobacco+ldl+adiposity+famhist+typea+obesity+alcohol+age,data=sa,family=binomial())
+
+summary(chdfit)
+
+exp(coef(chdfit))
+
+chdfac <- fitted(chdfit)
+thresh <- 0.5
+gchd <- cut(chdfac, breaks=c(-Inf, thresh, Inf), labels=c(0,1))
+
+confusionMatrix(gchd, sa$chd) 
+
+salda <- lda(as.factor(chd)~ sbp+tobacco+ldl+adiposity+famhist+typea+obesity+alcohol+age,data=sa, CV=T)
+confusionMatrix(salda$class, sa$chd)
+
+saqda <- qda(as.factor(chd)~ sbp+tobacco+ldl+adiposity+famhist+typea+obesity+alcohol+age,data=sa, CV=T)
+confusionMatrix(saqda$class, sa$chd)
