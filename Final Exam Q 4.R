cff396a403a334672ababed4af81d2f6ec41046b
Praveen Kumar Neelappa <pnkumar@outlook.com>
2017-10-18 10:02:19 -0400

    123

--- Final Exam Q 4.R
+++ Final Exam Q 4.R
@@ -26,9 +26,90 @@ for (i in 4:87)
 
 }
 
+#handle multivariate
+
+af <- read.csv("Asian_Fusion_Data.csv", as.is = TRUE);
+
+af <- af[,-c(59,66,67,68,71,72,73,74,75,76,77,78,79,80,81,82,83,84,99,100,101,102,103)]
+
+mydata <- na.omit(af)
+
+myfunction <- function(ads)
+{ 
+  x <- vector(mode="numeric", length=0)
+  y<- c(1:length(ads$Catkey))
+  for (i in y)
+  {
+    mdist <- mahalanobis(na.omit(ads[i,c(4:87)]),center=colMeans(na.omit(ads[-(i),c(4:87)])),cov(na.omit(ads[-(i),c(4:87)])) )
+    
+    if (length(mdist) > 0)
+    {
+      val2 <- 1-pchisq(mdist,84)
+      if(val2 < 0.01)
+      { 
+        x <- c(x, val2)
+      }
+      val2 <- NA
+    }
+    
+    else
+    {
+      #Do Nothin
+    }
+  }
+  print(x)
+}
+
+    
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x <- vector(mode="numeric", length=0)
+y<- c(1:length(mydata$Catkey))
+for (i in y)
+ {
+     mdist <- mahalanobis(na.omit(mydata[i,c(4:87)]),center=colMeans(na.omit(mydata[-(i),c(4:87)])),cov(na.omit(mydata[-(i),c(4:87)])) )
+     
+     if (length(mdist) > 0)
+     {
+         val2 <- 1-pchisq(mdist,7)
+         if(val2 < 0.01)
+         { 
+             x <- c(x, val2)
+         }
+     }
+     
+     else
+     {
+         #Do Nothin
+     }
+ }
+
+
+
+
+
+
+#junk code/reusable
 
 
 
